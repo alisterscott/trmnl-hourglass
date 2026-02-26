dbd5500606b00e20dc3c48dde2e485bf2f2455a2
Alister Scott <alister.scott@gmail.com>
2026-02-26 00:51:09 +0000

    Add support for start day of week

--- tests/hourglass.spec.ts
+++ tests/hourglass.spec.ts
@@ -81,3 +81,45 @@ test("can see 'Week' view across all the viewports", async ({ page }) => {
     fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
   }
 });
+
+test("can see 'Week' view across all the viewports when week starts on Sunday", async ({
+  page,
+}) => {
+  const sourceFile = path.join(__dirname, "week_sunday.trmnlp.yml");
+  const destFile = path.join(__dirname, "..", ".trmnlp.yml");
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`Source file not found: ${sourceFile}`);
+  }
+
+  if (!fs.existsSync(destFile)) {
+    throw new Error(`Destination file not found: ${destFile}`);
+  }
+
+  const originalContent = fs.readFileSync(destFile, "utf-8"); // Save original content to restore later
+
+  fs.copyFileSync(sourceFile, destFile);
+
+  try {
+    const routes = ["/quadrant", "/full", "/half_vertical", "/half_horizontal"];
+
+    for (const route of routes) {
+      await test.step(`Testing route: ${route}`, async () => {
+        await page.goto(route);
+        await page.getByRole("link", { name: "Poll" }).click();
+        const trmnlFrame = page.frameLocator("iframe");
+        await expect
+          .soft(trmnlFrame.locator('text[data-testing="label"]'))
+          .toHaveText("This Week");
+        await expect
+          .soft(trmnlFrame.locator("svg.hourglass"))
+          .toHaveAttribute("data-elapsed", "0.5");
+        await expect
+          .soft(trmnlFrame.locator("svg.hourglass"))
+          .toHaveAttribute("data-remaining", "0.5");
+      });
+    }
+  } finally {
+    fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
+  }
+});
