74f02cfd0a63b10ec9bd3243fde3c4eeec7cb100
Alister Scott <alister.scott@gmail.com>
2026-02-26 05:15:25 +0000

    Add custom label

--- tests/hourglass.spec.ts
+++ tests/hourglass.spec.ts
@@ -176,6 +176,49 @@ test("can see 'Day' with set hours after start time across all the viewports", a
   }
 });
 
+test("can see 'Day' with custom label across all the viewports", async ({
+  page,
+}) => {
+  const sourceFile = path.join(__dirname, "day_with_custom_label.trmnlp.yml");
+  const destFile = path.join(__dirname, "..", ".trmnlp.yml");
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`Source file not found: ${sourceFile}`);
+  }
+
+  if (!fs.existsSync(destFile)) {
+    throw new Error(`Destination file not found: ${destFile}`);
+  }
+
+  const originalContent = fs.readFileSync(destFile, "utf-8"); // Save original content to restore later
+
+  fs.copyFileSync(sourceFile, destFile);
+
+  try {
+    const routes = ["/quadrant", "/full", "/half_vertical", "/half_horizontal"];
+
+    for (const route of routes) {
+      await test.step(`Testing route: ${route}`, async () => {
+        await page.goto(route);
+        await page.getByRole("link", { name: "Poll" }).click();
+        const trmnlFrame = page.frameLocator("iframe");
+        await expect
+          .soft(trmnlFrame.locator('text[data-testing="label"]'))
+          .toHaveText("My Day");
+        await expect
+          .soft(trmnlFrame.locator("svg.hourglass"))
+          .toHaveAttribute("data-elapsed", "0.5");
+        await expect
+          .soft(trmnlFrame.locator("svg.hourglass"))
+          .toHaveAttribute("data-remaining", "0.5");
+        await expect.soft(trmnlFrame.locator("rect.fallingsand")).toBeVisible();
+      });
+    }
+  } finally {
+    fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
+  }
+});
+
 test("can see 'Week' view across all the viewports", async ({ page }) => {
   const sourceFile = path.join(__dirname, "week.trmnlp.yml");
   const destFile = path.join(__dirname, "..", ".trmnlp.yml");
